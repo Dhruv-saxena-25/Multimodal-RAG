9b705016f7a4321f514420a3eaadd8241a9d39fa
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-08-25 16:18:37 +0530

    app

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.schema.messages import HumanMessage, SystemMessage
-from langchain.schema.document import Document
+from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
-from langchain.retrievers.multi_vector import MultiVectorRetriever
-from fastapi import FastAPI, Request, Form, Response, File, UploadFile
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_core.output_parsers import StrOutputParser
-import json
+from langchain_core.runnables import RunnablePassthrough
 import os
-import uuid
-import base64
 from dotenv import load_dotenv
 load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
@@ -34,23 +27,20 @@ app.add_middleware(
 
 embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-db = FAISS.load_local("faiss_index", embedding, allow_dangerous_deserialization= True)
+vectorstore =FAISS.load_local("faiss_index", embedding, allow_dangerous_deserialization= True)
 
-prompt_template = """You are a vet doctor and an expert in analyzing dog's health.
+prompt_template = """You are an expert in maths question solving that contains specification, features, etc.
 Answer the question based only on the following context, which can include text, images and tables:
 {context}
 Question: {question}
 Don't answer if you are not sure and decline to answer and say "Sorry, I don't have much information about it."
 Just return the helpful answer in as much as detailed possible.
 Answer:
+
 """
-llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash", max_tokens=1024)
-prompt=PromptTemplate.from_template(prompt_template)
+model=ChatGoogleGenerativeAI(model="gemini-1.5-flash", max_tokens=1024)
+prompt=ChatPromptTemplate.from_template(prompt_template)
 
-qa_chain = LLMChain(llm=ChatGoogleGenerativeAI(model="gemini-1.5-pro", max_tokens=1024),
-                        prompt=PromptTemplate.from_template(prompt_template))
-
-# qa_chain = prompt | llm | StrOutputParser()
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -58,17 +48,22 @@ async def index(request: Request):
 
 @app.post("/get_answer")
 async def get_answer(question: str = Form(...)):
-    relevant_docs = db.similarity_search(question)
+    relevant_docs = vectorstore.similarity_search(question)
     context = ""
     relevant_images = []
     for d in relevant_docs:
-        if d.metadata['type'] == 'text': 
+        if d.metadata['type'] == 'text':
             context += '[text]' + d.metadata['original_content']
         elif d.metadata['type'] == 'table':
             context += '[table]' + d.metadata['original_content']
         elif d.metadata['type'] == 'image':
             context += '[image]' + d.page_content
             relevant_images.append(d.metadata['original_content'])
-    result = qa_chain.run({'context': context, 'question': question})
+    chain = (
+    {"context": lambda x: context, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser())
+    result= chain.invoke(question)
     return JSONResponse({"relevant_images": relevant_images[0], "result": result})
 
